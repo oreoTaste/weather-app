d0a9bf943302729950e68b3c9cf8ec834e423337
Youngkuk Sohn <youngkuk.sohn@gmail.com>
2020-08-18 20:40:57 +0900

    현재 날씨 및 5일치 기상예보 받기 구현

--- App.js
+++ App.js
@@ -1,13 +1,126 @@
 import React from "react";
+import axios from "axios";
 import { StyleSheet, Text, View } from "react-native";
+import * as Location from "expo-location";
+import Forecast from "./Forecast";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Hello</Text>
-    </View>
-  );
+const API_KEY = "115fb8e86a24d17553219d74ed2a0b37";
+class App extends React.Component {
+  state = {
+    isLoading: true,
+    current: {},
+    weathers: [],
+    city: {},
+    altitude: 0,
+    latitude: 0,
+    longitude: 0,
+    speed: 0,
+  };
+  //
+
+  getWeatherForecast = async ({ altitude, latitude, longitude, speed }) => {
+    // 현재 날씨 정보
+    // feels_like, temp, weather[0].main
+    const {
+      data: {
+        main: { feels_like, temp },
+        weather,
+      },
+    } = await axios.get(
+      `http://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${API_KEY}&units=metric`
+    );
+    console.log("현재날씨!!!! ", feels_like, temp);
+    var main;
+    console.log("현재날씨!!!! ", (main = weather[0].main));
+
+    // list:  3시간이내 예보, 1일/2일/3일/4일뒤 예보
+    // city: 도시정보
+    const {
+      data: { list, city },
+    } = await axios.get(
+      `http://api.openweathermap.org/data/2.5/forecast?lat=${latitude}&lon=${longitude}&appid=${API_KEY}&units=metric`
+    );
+
+    const weathers = [];
+    for (let i = 0; i < list.length; i += 8) {
+      weathers.push(list[i]);
+    }
+
+    latitude = Math.round(latitude * 10000000) / 10000000;
+    longitude = Math.round(longitude * 10000000) / 10000000;
+    console.log(latitude, longitude);
+
+    this.setState({
+      isLoading: false,
+      // 현재 날씨
+      current: {
+        feels_like,
+        temp,
+        main,
+      },
+      // 예보
+      weathers,
+      // 도시정보
+      city,
+      // 위치정보 (altitude, latitude, longitude,  speed)
+      altitude,
+      latitude,
+      longitude,
+      speed,
+    });
+  };
+
+  getLocation = async () => {
+    const { coords } = await Location.getCurrentPositionAsync();
+    this.getWeatherForecast(coords);
+  };
+
+  componentDidMount() {
+    this.getLocation();
+  }
+
+  render() {
+    const {
+      isLoading,
+      // 현재 날씨
+      current: { feels_like, temp, main },
+      // 예보
+      weathers,
+      // 도시정보
+      city,
+      // 위치정보 (altitude, latitude, longitude,  speed)
+      altitude,
+      latitude,
+      longitude,
+      speed,
+    } = this.state;
+
+    console.log(this.state);
+
+    return (
+      <View style={styles.container}>
+        {isLoading ? (
+          <Text>Loading...</Text>
+        ) : (
+          <>
+            <Text>
+              You're at {city.name}, {city.country}
+              <br />({latitude} / {longitude})
+              <br />
+              (고도: {altitude == null ? " - " : altitude} / 속도:
+              {speed == null ? " - " : spped})
+              <br />
+              현재날씨 :{feels_like} 같은 {temp}
+              도씨이며, {main}입니다
+            </Text>
+            <Forecast weathers={weathers} />
+          </>
+        )}
+      </View>
+    );
+  }
 }
+export default App;
 
 const styles = StyleSheet.create({
   container: {
---
+++ Forecast.js
@@ -0,0 +1,6 @@
+import React from "react";
+
+export default function Forecast({ weathers }) {
+  console.log(weathers);
+  return "";
+}
