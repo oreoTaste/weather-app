24990e62af1d86b3c70fd821b7e1684c78e2a15b
Youngkuk Sohn <youngkuk.sohn@gmail.com>
2020-08-18 16:12:17 +0900

    복습을 위한 초기화 설정

--- App.js
+++ App.js
@@ -1,58 +1,19 @@
-import React from 'react';
-import Loading from "./Loading";
-import * as Location from "expo-location";
-import { Alert } from 'react-native';
-import axios from "axios";
-import Weather from "./Weather";
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
 
-const API_KEY = "115fb8e86a24d17553219d74ed2a0b37";
-
-export default class extends React.Component {
-  state = {
-    isLoading : true
-  }
-
-  getWeather = async(latitude, longitude) => {
-    const {
-      data,
-      data: {
-        main: { temp },
-        weather,
-        name,
-        sys: {country}
-      }
-    } = await axios.get(
-      `http://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${API_KEY}&units=metric`
-    );
-    this.setState({isLoading: false, temp, condition: weather[0].main, name, country});
-  }
-
-  getLocation = async () => {
-    try {
-      await Location.requestPermissionsAsync();
-      const {
-         coords: {latitude, longitude, speed}
-        } = await Location.getCurrentPositionAsync();
-      
-      // send to API and get Weather
-      console.log(latitude, longitude);
-      const weather = this.getWeather(latitude, longitude);
-    } catch (error) {
-      Alert.alert("Can't find you", "So sad");
-    }
-  }
-
-  componentDidMount() {
-    this.getLocation();
-  }
-
-  render () {
-    const { isLoading, temp, condition, name, country} = this.state;
-    return (
-      isLoading ?
-      <Loading /> :
-      <Weather temp={Math.round(temp * 10) / 10} condition={condition} cityname={name} country={country}/>
-    );
-  }
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Hello</Text>
+    </View>
+  );
 }
 
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
--- readme.md
+++ readme.md
@@ -1,5 +1,3 @@
 # weather-app
 
 Learning React Native by Building a weather app
-
-expo url : https://expo.io/@youngkuk/weather-app
